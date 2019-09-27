8cd36861ee9969b9a7a6e5f98bd8f5ec83e59fd7
Ben Bariteau <ben.bariteau@gmail.com>
2019-09-27 15:57:10 -0700

    bump to v0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='context-helpers',
-    version='0.1.3',
+    version='0.1.4',
     description='Helpers for dealing with context managers',
     url='https://github.com/firba1/context-helpers',
     author='Ben Bariteau',
